df69297587ed60f49e8838abf646d0aafcf0d109
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 02:43:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 
 def oxford_comma array
   if array.size == 1
-    return arra.join('')
+    return array.join('')
       elsif array.size == 2
        return array.join(' and ')
         elsif array.size >= 3
